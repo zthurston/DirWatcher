2404b349d6c835acfa0da9a6051d5b1f95d14a0b
Zach Thurston <zach@zthurston.com>
2019-08-14 20:28:11 -0600

    Integrate DirWatcher into console app
    - Configure a DirWatcher when valid arguments come through
    - Use CancellationToken/Source to cancel scanning when the user presses the cancel sequence
    - Echo what DirWatcher finds to Console

--- DirWatcher/Program.cs
+++ DirWatcher/Program.cs
@@ -1,15 +1,72 @@
-﻿using CommandLine;
-using System;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CommandLine;
+using DirWatcher.Models;
 
 namespace DirWatcher
 {
     class Program
     {
-        static void Main(string[] args)
+        const int ScanIntervalMs = 1_000;
+        static CancellationTokenSource CancellationTokenSource;
+        static IDirWatcher Watcher;
+
+        static async Task<int> Main(string[] args)
         {
-            IDirWatcher watcher = new DirWatcher();
+
+            CancellationTokenSource = new CancellationTokenSource();
+            Console.CancelKeyPress += Console_CancelKeyPress;
             Parser.Default.ParseArguments<CommandLineOptions>(args)
-                .WithParsed(o => watcher.Run(o));
+                .WithParsed(ConfigureWatcher);
+
+            if (Watcher == null)
+                return -1;
+
+            await RunWatcher();
+            return 0;
+        }
+
+        private static async Task RunWatcher()
+        {
+            try
+            {
+                await Task.Run(() => Watcher.Run(CancellationTokenSource.Token));
+            }
+            finally
+            {
+                Cleanup();
+            }
+
+            Console.WriteLine("Finished");
+        }
+
+        private static void ConfigureWatcher(CommandLineOptions options)
+        {
+            Watcher = new DirWatcher(TimeSpan.FromMilliseconds(ScanIntervalMs),
+                       options.Directory, options.WatchPattern);
+
+            Watcher.OnDirectoryScanned += OnDirectoryScanned;
+        }
+
+        private static void OnDirectoryScanned(DirectoryScannedEventArgs eventArgs)
+        {
+            Console.WriteLine($"Found files: {string.Join(' ', eventArgs.FilePaths)} in {eventArgs.Directory} at {eventArgs.TimeStamp.ToLocalTime()}");
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine("Cancelling watch...");
+            CancellationTokenSource.Cancel();
+        }
+
+        private static void Cleanup()
+        {
+            // NOTE: not strictly necessary, as the process ending will
+            // clean everything up anyway.
+            Console.CancelKeyPress -= Console_CancelKeyPress;
+            Watcher.OnDirectoryScanned -= OnDirectoryScanned;
         }
     }
 }
